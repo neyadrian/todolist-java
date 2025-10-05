c881b63b3a2c565711795f64eb8e780b18721353
neyadrian <neyadrian2018@gmail.com>
2025-10-05 08:44:46 -0300

    Updates

---
+++ todolist/todolist/src/main/java/br/com/neyadrian/todolist/user/UserController.java
@@ -0,0 +1,28 @@
+package br.com.neyadrian.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+    } 
+}
