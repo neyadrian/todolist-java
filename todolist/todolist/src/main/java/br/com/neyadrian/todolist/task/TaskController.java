eae2483c5c7bbd7deae13312829afc7682f8aaf9
neyadrian <neyadrian2018@gmail.com>
2025-10-14 19:42:45 -0300

    update

--- todolist/todolist/src/main/java/br/com/neyadrian/todolist/task/TaskController.java
+++ todolist/todolist/src/main/java/br/com/neyadrian/todolist/task/TaskController.java
@@ -1,18 +1,23 @@
 package br.com.neyadrian.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import jakarta.servlet.http.HttpServletRequest;
+
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -36,5 +41,20 @@ public class TaskController {
         }
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        taskModel.setId(id);
+        return this.taskRepository.save(taskModel);
     }
 }
